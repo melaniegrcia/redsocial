ce4ff87ef668f98c461cf0a4904ea8a17ace29cf
melaniegrcia <mel30.mg@gmail.com>
2019-05-20 21:37:38 -0300

    cambios

---
+++ paginas/codigos jquery/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("#button-post").click(function() {
+        $("#post").append(
+            '<div class="post-estado">'+
+            '<input class="form-control" type="text" placeholder="Post Estado">'+
+            '</div>'
+                 )
+    })
+            
+})
