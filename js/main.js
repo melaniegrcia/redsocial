6b499d8d236bb8ee941334ea30031223e141c8a6
adriancolman <urielaraujo125@gmail.com>
2019-05-31 21:57:44 -0300

    agrego el js de melanie y poco mas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+
+    alert("afkgakbjvkfddfadkgsfahdsfhgasfjdafj")
+
      // Codigo a ejecutarse una vez que se carga la pagina
      $("#post-button").click(function() {
          $("#contenedor-principal").append(
@@ -26,4 +30,88 @@ $(document).ready(function() {
         //a la columna de post, con los datos que hayamos agregado
          );
      });
+
+
+
+
+          // Muestra el formulario de login
+          $("#ir-a-login").click(function(){
+            $("#form-registro").hide();
+            $("#form-ingreso").show();
+        });
+    
+        // Muestra el formulario de registro
+        $("#ir-a-registro").click(function(){
+            $("#form-registro").show();
+            $("#form-ingreso").hide();
+        }); 
+
+    
+        // condiciones para registrarse
+        $("#boton-registro").click(function(){
+
+            alert("click en registro");
+            console.log("CLICK EN REGISTRO");  
+    
+            var usuario = "pablo";
+            var password = "comit2019";
+            var inputPassword = $("#login-password").val();
+            var inputUsuario = $("#login-user").val();
+    
+            
+            if (inputUsuario === "" || 
+                inputPassword === "") {
+                alert("Por favor complete todos los campos")
+            } else {
+                if (inputUsuario === usuario && 
+                    inputPassword === password) {
+                    alert("Login correcto");
+                } else {
+                   alert("Login incorrecto");
+                }
+            }
+
+            
+        });
+    
+        $("#boton-ingreso").click(function(){
+
+            alert("click en ingreso")
+    
+            var usuario = "pablo";
+            var password = "comit2019";
+            var inputPassword = $("#login-password").val();
+            var inputUsuario = $("#login-user").val();
+    
+            
+            if (inputUsuario === "" || 
+                inputPassword === "") {
+                alert("Por favor complete todos los campos")
+            } else {
+                if (inputUsuario === usuario && 
+                    inputPassword === password) {
+                    alert("Login correcto");
+                    location.href="home.html"; // location.href sirve para redireccionar a otra pagina ;D
+                } else {
+                   alert("Login incorrecto");
+                }
+            }
+
+            
+        });
+    
+
+        $("#btn-mostrar").click(function(){
+            alert("click mostrar");
+            $("#series-vistas").hide(); 
+        });
+    //$("#series-vistas").hide();
+    //$(".brn.mostrar").on('click', mostrar);
+    //function mostrar(){
+     //   $("#series-vistas").hide();
+   //     console.log(this);
+   // }
 });
+
+
+
