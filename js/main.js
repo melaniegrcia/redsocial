ae2de4b64319913df8a7e02efd9fad4598d1452d
melanie grcia <mel30.mg@gmail.com>
2019-06-03 17:59:59 -0300

    cambios en chat y estilos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
+     // Muestra el formulario de login
+     $("#ir-a-login").click(function(){
+        $("#form-registro").hide();
+        $("#form-ingreso").show();
+    });
+
+    // Muestra el formulario de registro
+    $("#ir-a-registro").click(function(){
+        $("#form-registro").show();
+        $("#form-ingreso").hide();
+    }); 
+
+    $("#boton-registro").click(function(){
+
+        var usuario = "pablo";
+        var password = "comit2019";
+        var inputPassword = $("#login-password").val();
+        var inputUsuario = $("#login-user").val();
+
+        
+        if (inputUsuario === "" || 
+            inputPassword === "") {
+            alert("Por favor complete todos los campos")
+        } else {
+            if (inputUsuario === usuario && 
+                inputPassword === password) {
+                alert("Login correcto");
+            } else {
+               alert("Login incorrecto");
+            }
+        }
+    });
+
+    $("#botonlogin").click(function(){
+
+        var usuario = "pablo";
+        var password = "comit2019";
+        var inputPassword = $("#login-password").val();
+        var inputUsuario = $("#login-user").val();
+
+        
+        if (inputUsuario === "" || 
+            inputPassword === "") {
+            alert("Por favor complete todos los campos")
+        } else {
+            if (inputUsuario === usuario && 
+                inputPassword === password) {
+                alert("Login correcto");
+            } else {
+               alert("Login incorrecto");
+            }
+        }
+    });
+});
      // Codigo a ejecutarse una vez que se carga la pagina
      $("#post-button").click(function() {
          $("#contenedor-principal").append(
@@ -21,5 +76,10 @@ $(document).ready(function() {
         //todo lo anterior, cada vez q presiono publicar, agrega una caja de post
         //a la columna de post, con los datos que hayamos agregado
          );
+
      });
-});
+     
+    
+
+
+    //  if ($("#password").val() === "commit2019")
